d57350c52f00d8f0069d8fd7db52fd75948a4706
doyeoo <ehdusla013@gmail.com>
2023-02-09 22:44:09 +0900

    initial commit

---
+++ src/main/java/org/zerock/voco/VocoApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.voco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/zerock/voco/VocoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.voco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
