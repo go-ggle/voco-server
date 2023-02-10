5282c0daa8b0aec6033e67d7e8a521f4f564a2ca
doyeoo <ehdusla013@gmail.com>
2023-02-10 17:58:02 +0900

    feat: Train 클래스

---
+++ src/main/java/com/goggle/voco/domain/Train.java
@@ -0,0 +1,23 @@
+package com.goggle.voco.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Train extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private int text_id;
+    private String text;
+
+}
