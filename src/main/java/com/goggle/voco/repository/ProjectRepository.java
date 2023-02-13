a1ed0b563d66308143964200bd2c4005f9e952f7
doyeoo <ehdusla013@gmail.com>
2023-02-13 22:06:04 +0900

    feat: 리포지토리 추가

--- src/main/java/com/goggle/voco/repository/ProjectRepository.java
+++ src/main/java/com/goggle/voco/repository/ProjectRepository.java
@@ -3,5 +3,8 @@ package com.goggle.voco.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.goggle.voco.domain.Project;
 
+import java.util.Optional;
+
 public interface ProjectRepository extends JpaRepository<Project, Long>{
+    Optional<Project> findById(Long projectId);
 }
