68d890ef5a809af30553a527a3b823bf9fa327e4
doyeoo <81256252+doyeoo@users.noreply.github.com>
2023-02-24 18:03:25 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,20 +1,20 @@
-REPOSITORY=/home/ubuntu/build/libs
- 
-cd $REPOSITORY
- 
-JAR_NAME=$(ls $REPOSITORY | grep '*SNAPSHOT.war' | tail -n 1)
-JAR_PATH=$REPOSITORY/$JAR_NAME
- 
+REPOSITORY=/home/ubuntu
+
+echo "> 빌드 파일 복사"
+cp $REPOSITORY/build/libs/*SNAPSHOT.war $REPOSITORY/
+
+echo "> 프로세스 아이디 확인"
 CURRENT_PID=$(pgrep -f *.war)
- 
+
 if [ -z $CURRENT_PID ]
 then
-  echo "> Nothing to end."
+  echo "> 실행중인 프로세스 없음"
 else
   echo "> kill -9 $CURRENT_PID"
   kill -15 $CURRENT_PID
   sleep 5
 fi
- 
-echo "> $JAR_PATH deploy"
-nohup java -jar $JAR_PATH --spring.profiles.active=dev > /dev/null 2> /dev/null < /dev/null &
+
+echo "> 새 애플리케이션 배포"
+JAR_NAME=$(ls -tr $REPOSITORY/ | grep war | tail -n 1)
+nohup java -jar $REPOSITORY/$JAR_NAME --spring.profiles.active=dev > /dev/null 2> /dev/null < /dev/null &
