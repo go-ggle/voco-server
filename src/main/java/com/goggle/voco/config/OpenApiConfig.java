f8579450ab38196330cbb8ade2e5c1c3fa97c0d9
doyeoo <ehdusla013@gmail.com>
2023-03-29 13:53:11 +0900

    feat: 스웨거 요청 토큰 헤더 적용

--- src/main/java/com/goggle/voco/config/OpenApiConfig.java
+++ src/main/java/com/goggle/voco/config/OpenApiConfig.java
@@ -3,8 +3,13 @@ package com.goggle.voco.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 @Configuration
 public class OpenApiConfig {
@@ -14,8 +19,15 @@ public class OpenApiConfig {
                 .title("VOCO API Document")
                 .version("v0.0.1")
                 .description("VOCO API 명세서입니다.");
+
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
         return new OpenAPI()
-                .components(new Components())
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .security(Arrays.asList(securityRequirement))
                 .info(info);
     }
 }
