06cae8d1c7161663ba68176277317879bb6b1dc6
aqswa <joayoung3@gmail.com>
2023-03-30 19:48:13 +0900

    refactor: traindata dto setter 제거

--- src/main/java/com/goggle/voco/dto/TrainDataResponseDto.java
+++ src/main/java/com/goggle/voco/dto/TrainDataResponseDto.java
@@ -1,5 +1,7 @@
 package com.goggle.voco.dto;
 
+import com.goggle.voco.domain.Block;
+import com.goggle.voco.domain.Train;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,4 +17,11 @@ public class TrainDataResponseDto {
     private int text_id;
     private String text;
 
+    public static TrainDataResponseDto from(Train train) {
+        return new TrainDataResponseDto(
+                train.getId(),
+                train.getTextId(),
+                train.getText()
+        );
+    }
 }
