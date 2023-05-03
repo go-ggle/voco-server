11e7eb3b0e30e3330ad9a661800c98adaee6b39e
aqswa <joayoung3@gmail.com>
2023-05-03 16:19:09 +0900

    feat: train 완료 시 함수로 뺌

--- src/main/java/com/goggle/voco/service/TrainService.java
+++ src/main/java/com/goggle/voco/service/TrainService.java
@@ -3,5 +3,6 @@ package com.goggle.voco.service;
 import com.goggle.voco.dto.TrainRequestDto;
 
 public interface TrainService {
+    void finishTrain(Long userId);
     void startTrain(TrainRequestDto trainRequestDto) throws Exception;
 }
